1b0fd2bbfd8e74621f06777689a59ac684dcc7a3
Navarr Barnier <me@navarr.me>
2012-07-30 20:06:31 -0400

    Additional Work on Chat System

--- protected/controllers/ChatController.php
+++ protected/controllers/ChatController.php
@@ -6,6 +6,9 @@ class ChatController extends Controller
 	{
 		return array(
 			'accessControl',
+			array(
+				'BanFilter',
+			),
 		);
 	}
 
@@ -13,11 +16,11 @@ class ChatController extends Controller
 	{
 		return array(
 			array('allow',
-				'actions'=>array('index','new','post','ping'),
+				'actions'=>array('index','view','new','post','ping'),
 				'users' => array('@'),
 			),
 			array('deny',
-				'actions'=>array('index','new','post','ping'),
+				'actions'=>array('index','view','new','post','ping'),
 				'users'=>array('*')
 			),
 		);
@@ -25,7 +28,13 @@ class ChatController extends Controller
 
 	public function actionIndex()
 	{
-		$messages = ChatMessage::model()->with('user')->findByAttributes(array('room' => 'lobby'),array(
+		// Until we actually make a multi-room system.
+		$this->actionView('lobby');
+	}
+
+	public function actionView($room = 'lobby')
+	{
+		$messages = ChatMessage::model()->with('user')->findAllByAttributes(array('room' => $room),array(
 			'condition' => 'timestamp > NOW()-60*60*6',
 			'limit' => 10,
 		));
@@ -33,5 +42,56 @@ class ChatController extends Controller
 		$this->render('index',array(
 			'messages' => $messages,
 		));
+	}
+
+	public function actionNew($since,$room = 'lobby')
+	{
+		$messages = ChatMessage::model()->with('user')->findAllByAttributes(array('room' => $room),'id > :since',array('since' => $since));
+		$output = array();
+		foreach($messages as $message)
+		{
+			$output[] = array(
+				'id' => $message->id,
+				'message' => $message->message,
+				'user' => array(
+					'id' => $message->user->id,
+					'ign' => $message->user->ign,
+					'type' => $message->user->type,
+					'rank' => $message->user->rank,
+				),
+			);
+		}
+		$this->jsonOut($output);
+	}
+	
+	public function actionPing($room = 'lobby')
+	{
+		$this->jsonOut('Not Supported',501);
+	}
+
+	public function actionPost($room = 'lobby')
+	{
+		$model = new ChatForm;
+		if(isset($_POST['ChatForm']))
+		{
+			$model->attributes=$_POST['ChatForm'];
+			if($model->validate())
+			{
+				$message = new ChatMessage;
+				$message->userID = Yii::app()->user->getId();
+				$message->message = $model->message;
+				$message->room = $room;
+				$success = $message->save();
+				if(Yii::app()->request->isAjaxRequest) $this->jsonOut(array(
+					'success' => $success,
+					'message' => $success ? $message : null,
+					'csrf' => array(
+						'name' => Yii::app()->request->csrfTokenName,
+						'value' => Yii::app()->request->csrfToken,
+					),
+				));
+			}
+		}
+		$this->redirect(array('chat/index'));
 	}
 }
--- protected/forms/ChatForm.php
+++ protected/forms/ChatForm.php
@@ -2,5 +2,13 @@
 
 class ChatForm extends CFormModel
 {
-	
+	public $message;
+
+	public function rules()
+	{
+		return array(
+			array('message','required'),
+			array('message','length','min'=>5),	
+		);
+	}
 }
