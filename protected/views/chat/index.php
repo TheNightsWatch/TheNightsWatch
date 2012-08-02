687003323bd0c6260572558553368b9cc0899c82
Navarr Barnier <me@navarr.me>
2012-08-01 22:58:01 -0400

    Fix for the lack of zero-padding on hours, minutes, and seconds

--- protected/views/chat/index.php
+++ protected/views/chat/index.php
@@ -16,15 +16,36 @@ function messageQueue()
 	this.lastID = <?php echo $lastID; ?>;
 	this.currentTimeout = undefined;
 }
+Date.prototype.getPaddedMinutes = function(_this)
+{
+	if(_this === undefined) _this = this;
+	var m = _this.getMinutes();
+	if(m < 10) m = '0' + m;
+	return m;
+}
+Date.prototype.getPaddedHours = function(_this)
+{
+	if(_this === undefined) _this = this;
+	var h = _this.getHours();
+	if(h < 10) h = '0' + h;
+	return h;
+}
+Date.prototype.getPaddedSeconds = function(_this)
+{
+	if(_this === undefined) _this = this;
+	var s = _this.getSeconds();
+	if(s < 10) s = '0' + s;
+	return s;
+}
 Date.prototype.getSmallChatStamp = function(_this)
 {
 	if(_this === undefined) _this = this;
-	return _this.getHours() + ':' + _this.getMinutes();
+	return _this.getPaddedHours() + ':' + _this.getPaddedMinutes();
 }
 Date.prototype.getLargeChatStamp = function(_this)
 {
 	if(_this === undefined) _this = this;
-	return _this.getHours() + ':' + _this.getMinutes() + ':' + _this.getSeconds();
+	return _this.getPaddedHours() + ':' + _this.getPaddedMinutes() + ':' + _this.getPaddedSeconds();
 }
 messageQueue.prototype.initialLoad = function()
 {
