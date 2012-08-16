4d6337f359afc77cf03be4494536a19d5c6f92cb
Navarr Barnier <me@navarr.me>
2012-08-16 00:50:01 -0400

    Mod Page Update

--- protected/views/site/mods.php
+++ protected/views/site/mods.php
@@ -1,11 +1,27 @@
 <p>There are several modifications that are recommended by the Council in order to best play MineZ and for best activity within the Watch:</p>
 <ul>
     <li><a href="http://www.minecraftforum.net/topic/232701-">McPatccher</a> - A tool, not a mod.  Used to easily patch mods into the Minecraft jar, in the event ModLoader is being a bitch.</li>
-    <li><a href="http://www.minecraftforum.net/topic/72747-">Faithful 32</a> - A 32-bit texture pack that keeps the Minecraftian feel.  Requires HD Textures.  Will work with provided JAR.</li>
     <li><a href="http://www.minecraftforum.net/topic/75440-">ModLoader</a> - A mod that helps other mods.  Most mods you install will need this.</li>
+    <li><a href="http://www.minecraftforum.net/topic/249637-">Optifine</a> - Improves Minecraft Performance</li>
     <li><a href="http://www.minecraftforum.net/topic/482147-">Rei's Minimap</a> - Shows the miniature map that most other MineZ players use.  Allows you to set waypoints.</li>
-    <li><a href="http://www.minecapes.net/">MineCapes</a> - Add the <a href="http://i.imgur.com/PW6NX.png">Night's Watch Cape</a> for immediate recognition amongst your peers.</li>
+    <li><a href="http://www.minecraftforum.net/topic/72747-">Faithful 32</a> - A 32-bit texture pack that keeps the Minecraftian feel.  Requires HD Textures.  Will work with provided JAR.</li>
     <li><a href="http://www.minecraftforum.net/topic/986083-">FancyGUI</a> - Makes MineCraft's normal pages prettier, and gives you a nifty friend's list.</li>
-</ul> 
-<p>If you don't want to play with modding around, we've made a <a href="<?php echo $this->createUrl('site/modDownload'); ?>">a nifty minecraft.jar [1.3.1]</a> (<strong>Note:</strong> This jar is probably incompatible with anything you have in the <tt>mods</tt> folder.  If you are having problems, try renaming or moving your <tt>mods</tt> folder.) that has all of it for you - and don't worry!  It's protected.  You can only access it if you're a member of the watch and a Minecraft Premium user.  Just stick it in <tt><a href="file:///%APPDATA%/.minecraft/bin">%APPDATA%/.minecraft/bin</a></tt> (overwriting your previous installation) and you're ready to go!</p>
-<p><small>Note: Our modified version also includes HD Textures, HD Font, Random Mobs, Custom Colors, Connected Textures, and Better Skies.</small></p> 
+    <li><a href="http://www.minecraftforum.net/topic/1114612-">Status Effect HUD</a> - Shows what's ailing you at any time on screen</li>
+    <li><a href="http://www.minecapes.net/">MineCapes</a> - Set your own cape and see your friends'.</li>
+</ul>
+<p>If you don't want to play with modding around, we've made a <a href="<?php echo $this->createUrl('site/modDownload'); ?>">a nifty minecraft.jar [1.3.1]</a> that has all of it for you - and don't worry!  It's protected.  You can only access it if you're a member of the watch and a Minecraft Premium user.  Just stick it in <tt><a href="file:///%APPDATA%/.minecraft/bin">%APPDATA%/.minecraft/bin</a></tt> (overwriting your previous installation) and you're ready to go!</p>
+
+<p><strong>To add a MineCape:</strong></p>
+<p>Goto the <a href="http://www.minecapes.net/upload">>MineCape upload page</a> and choose import from URL. Add the URL of the cape: <a href="http://i.imgur.com/PW6NX.png">http://i.imgur.com/PW6NX.png</a> and then follow the instructions on screen for how to activate the cape. Make sure you have the MineCape mod installed.</p>
+
+<p><strong>Notes about using on MineZ:</strong></p>
+<p>All of the above mods are currently all allowed on the MineZ servers. If you see ANYTHING about ANY of these mods not being allowed, TELL US RIGHT AWAY. We do not want to be encouraging users to use illegal mods.</p>
+
+<p><strong>If the JAR is not working for you:</strong></p>
+<p>First, make sure your ".minecraft/mods" folder has no .ZIP files in it. If it does, we assume you know what you are doing and you are on you own. Otherwise, please goto all the threads/website for these and read all the FAQs for them and see if any of those problems cover yours. If you are still having problems and Minecraft is crashing, run Minecraft, and right AFTER it crashes, open ".minecraft/crash-reports/" and mod mail us on <a href="http://www.reddit.com/message/compose?to=%2Fr%2FTheNightsWatch">/r/TheNightsWatch</a> with what you were doing in the game to cause it to crash. There is no promise we can help fix you problems.</p>
+
+<p><strong>Add your own mods:</strong></p>
+<p>If you have any recommendations to add to this list or need help adding other mods to your own client, mod mail us on <a href="http://www.reddit.com/message/compose?to=%2Fr%2FTheNightsWatch">/r/TheNightsWatch</a> and Angellus_Mortis will help you. Note: we will NOT help you install mods that are illegal on the MineZ servers.</p>
+
+<p><strong>Disclaimer:</strong></p>
+<p>The Night's Watch did not write any of this code, nor do we own it. We are just giving it out to help our members. All the above mods are owned by the respective makers and you should go and read the forum threads for those mods if you have any problems or questions before asking us. Minecraft is owned by Mojang. All rights belong to them. Please support the original mod makers and Mojang as all of them put a lot of effort into these.</p>
