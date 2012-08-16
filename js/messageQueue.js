f563bfdbc2e94122abf829cddd71625ac58d62e2
Navarr Barnier <me@navarr.me>
2012-08-15 21:51:57 -0400

    Desktop Notifications in Chat

--- js/messageQueue.js
+++ js/messageQueue.js
@@ -1,84 +1,135 @@
-function messageQueue(lastID,pingURL)
-{
+function messageQueue(lastID, pingURL) {
 	this.lastID = lastID;
 	this.pingURL = pingURL;
 	this.currentTimeout = undefined;
-}
-messageQueue.prototype.initialLoad = function(_this)
-{
-	if(this.currentTimeout) clearTimeout(this.currentTimeout);
+	this.focused = true;
+
 	_this = this;
-	_this.currentTimeout = setTimeout(function() { _this.load(_this); },2000);
+
+	// Is the user focused on our window?
+	$(document).ready(function() {
+		$(window).on('focus', function() {
+			_this.focused = true;
+			// hide notifications
+		});
+		$(window).on('blur', function() {
+			_this.focused = false;
+		});
+	});
+}
+messageQueue.prototype.initialLoad = function(_this) {
+	if (this.currentTimeout)
+		clearTimeout(this.currentTimeout);
+	_this = this;
+	_this.currentTimeout = setTimeout(function() {
+		_this.load(_this);
+	}, 2000);
 };
-messageQueue.prototype.load = function(_this,callback)
-{
-	if(_this === undefined) _this = this;
-	if(_this.currentTimeout) clearTimeout(_this.currentTimeout);
-	$.getJSON(_this.pingURL + '?since=' + _this.lastID,function(data)
-	{
-		setTimeout(function()
-		{
+messageQueue.prototype.load = function(_this, callback) {
+	if (_this === undefined)
+		_this = this;
+	if (_this.currentTimeout)
+		clearTimeout(_this.currentTimeout);
+	$.getJSON(_this.pingURL + '?since=' + _this.lastID, function(data) {
+		setTimeout(function() {
 			var nuserol = $('#chatters').clone();
 			nuserol.html('');
-			for(var i in data.users)
-			{
+			for ( var i in data.users) {
 				var li = $(document.createElement("li"));
 				var a = $(document.createElement("a"));
-				a.attr('href',data.users[i].url).addClass('username').attr('title',data.users[i].title);
+				a.attr('href', data.users[i].url).addClass('username').attr(
+						'title', data.users[i].title);
 				var img = $(document.createElement("img"));
-				img.attr('src',data.users[i].img);
+				img.attr('src', data.users[i].img);
 				a.append(img);
 				a.html(a.html() + ' ' + data.users[i].ign);
 				li.append(a);
 				nuserol.append(li);
 			}
 			$('#chatters').html(nuserol.html());
-		},1);
-		
-		if(!data.messages.length) return;
+		}, 1);
+
+		if (!data.messages.length)
+			return;
 		var ele = $('#template').clone();
 		ele.removeAttr('id');
 		var scroll = _this.getShouldScroll();
-		for(var i in data.messages)
-		{
-			if(data.messages[i].id <= _this.lastID) continue;
-			_this.lastID = data.messages[i].id;
-			
-			var li = ele.clone();
-			var date = new Date(data.messages[i].timestamp*1000);
-			li.find('.timestamp').attr('title',date.getLargeChatStamp()).text(date.getSmallChatStamp());
-			li.find('.username').text(data.messages[i].user.ign).attr('href',data.messages[i].user.url).attr('title',data.messages[i].user.title);
-			li.find('.message').html(data.messages[i].html);
-			$('#messages').append(li);
-			if(data.messages[i].user.ign != document.myIGN)
-			{
-				document.titleBar.flashMessage(data.messages[i].user.ign);
-				setTimeout(function() { if(window.webkitNotifications)
-				{
-					if(window.webkitNotifications.checkPermission() != 0) return;
+		for ( var i in data.messages) {
+			if (data.messages[i].id <= _this.lastID)
+				continue;
 
-					var notif = window.webkitNotifications.createNotification(data.messages[i].user.img,data.messages[i].user.ign,data.messages[i].text);
-					notif.ondisplay = function() { setTimeout(function() { notif.cancel(); },3000); };
-					notif.show();
-				}; },1);
-			};
+			var mData = data.messages[i];
+
+			_this.lastID = mData.id;
+
+			var li = ele.clone();
+			var date = new Date(mData.timestamp * 1000);
+			li.find('.timestamp').attr('title', date.getLargeChatStamp()).text(
+					date.getSmallChatStamp());
+			li.find('.username').text(mData.user.ign).attr('href',
+					mData.user.url).attr('title', mData.user.title);
+			li.find('.message').html(mData.html);
+			$('#messages').append(li);
+			if (data.messages[i].user.ign != document.myIGN) {
+				document.titleBar.flashMessage(mData.user.ign);
+
+				var doThings = function(mData) {
+					setTimeout(function() {
+						if (!window.Notification)
+							return;
+						if (typeof (Storage) == "undefined")
+							return;
+						if (localStorage['notify_chat'] == "none")
+							return;
+						if (Notification.permissionLevel() != "granted")
+							return;
+
+						var options = {};
+						options.body = mData.text;
+						options.iconUrl = mData.user.img;
+						if (localStorage['notify_chat'] == 'show')
+							options.tag = 'notif-' + mData.id;
+						if (localStorage['notify_chat'] == 'collapse')
+							options.tag = 'notif';
+
+						console.log(options.tag);
+
+						var n = webkitNotifications.createNotification(
+								options.iconUrl, mData.user.ign, options.body);
+						n.replaceId = options.tag;
+						n.show();
+
+						if (localStorage['notify_chat'] == 'collapse_perm')
+							return;
+						setTimeout(function() {
+							n.cancel();
+						}, 3000);
+					}, 1);
+				};
+				doThings(mData);
+			}
 		}
 
-		$('#chatForm .csrf').attr('name',data.csrf.name).attr('value',data.csrf.token);
-		if(scroll) _this.doScroll();
-		if(callback !== undefined) callback();
+		$('#chatForm .csrf').attr('name', data.csrf.name).attr('value',
+				data.csrf.token);
+		if (scroll)
+			_this.doScroll();
+		if (callback !== undefined)
+			callback();
 	});
-	_this.currentTimeout = setTimeout(function() { _this.load(_this); },2000);
+	_this.currentTimeout = setTimeout(function() {
+		_this.load(_this);
+	}, 2000);
 };
-messageQueue.prototype.getShouldScroll = function()
-{
+messageQueue.prototype.getShouldScroll = function() {
 	var height = $('#messages').height();
 	var scrollHeight = $('#messages').prop('scrollHeight');
-	if(scrollHeight-height == $('#messages').prop('scrollTop'))
+	if (scrollHeight - height == $('#messages').prop('scrollTop'))
 		return true;
 	return false;
 };
-messageQueue.prototype.doScroll = function()
-{
-	$('#messages').prop({scrollTop:$('#messages').prop('scrollHeight')});
+messageQueue.prototype.doScroll = function() {
+	$('#messages').prop({
+		scrollTop : $('#messages').prop('scrollHeight')
+	});
 };
--- protected/views/chat/index.php
+++ protected/views/chat/index.php
@@ -1,22 +1,42 @@
 <?php
-	Yii::app()->clientScript->registerCoreScript('jquery');
-	Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/Date.js');
-	Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/titleBar.js');
-	Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/messageQueue.js?v=20120810v1');
-	$lastID = 0; 
+Yii::app()->clientScript->registerCoreScript('jquery');
+Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/Date.js');
+Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/titleBar.js');
+Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/messageQueue.js?v=20120810v1');
+$lastID = 0;
 ?>
 <ol id="messages">
-	<li id="template">[<span class="timestamp" data-epoch=""></span>] &lt;<a href="" class="username"></a>&gt; <span class="message"></span></li>
+	<li id="template">[<span class="timestamp" data-epoch=""></span>] &lt;<a
+		href="" class="username"></a>&gt; <span class="message"></span>
+	</li>
 	<?php foreach($messages as $message): $lastID = $message->id; ?>
-		<li>[<span class="timestamp" data-epoch="<?php echo $message->timestamp->getTimestamp(); ?>" title="<?php echo $message->timestamp->format("H:i:s"); ?>"><?php echo $message->timestamp->format("H:i"); ?></span>] &lt;<a href="<?php echo $this->createUrl('user/view',array('unique' => $message->user->ign)); ?>" class="username" title="<?php echo htmlspecialchars($message->user->title); ?>"><?php echo htmlspecialchars($message->user->ign); ?></a>&gt; <span class="message"><?php echo TextParser::parse(htmlspecialchars($message->message)); ?></span></li>
+	<li>[<span class="timestamp"
+		data-epoch="<?php echo $message->timestamp->getTimestamp(); ?>"
+		title="<?php echo $message->timestamp->format("H:i:s"); ?>"><?php echo $message->timestamp->format("H:i"); ?>
+	</span>] &lt;<a
+		href="<?php echo $this->createUrl('user/view',array('unique' => $message->user->ign)); ?>"
+		class="username"
+		title="<?php echo htmlspecialchars($message->user->title); ?>"><?php echo htmlspecialchars($message->user->ign); ?>
+	</a>&gt; <span class="message"><?php echo TextParser::parse(htmlspecialchars($message->message)); ?>
+	</span>
+	</li>
 	<?php endforeach; ?>
 </ol>
-<form id="chatForm" action="<?php echo $this->createUrl('chat/post'); ?>" method="post">
-	<input class="csrf" type="hidden" name="<?php echo Yii::app()->request->csrfTokenName ?>" value="<?php echo Yii::app()->request->csrfToken ?>" />
-	<input type="text" autocomplete="off" name="ChatForm[message]" id="message" />
+<form id="chatForm"
+	action="<?php echo $this->createUrl('chat/post'); ?>" method="post">
+	<input class="csrf" type="hidden"
+		name="<?php echo Yii::app()->request->csrfTokenName ?>"
+		value="<?php echo Yii::app()->request->csrfToken ?>" /> <input
+		type="text" autocomplete="off" name="ChatForm[message]" id="message" />
 	<button disabled>send</button>
 </form>
-<!--  <button id="enableNotifs" style="display:none;">Enable Desktop Notifications</button> -->
+<label id="enableNotifs" style="display: none">Desktop Notifications: <select
+	id="notify_chat"><option value="none">None</option>
+		<option value="show">Show</option>
+		<option value="collapse">Collapse (And Hide)</option>
+		<option value="collapse_perm">Collapse (And Keep)</option>
+</select>
+</label>
 <script>
 document.myIGN = <?php echo json_encode(User::model()->findByPk(Yii::app()->user->getId())->ign); ?>;
 document.titleBar = new titleBar();
@@ -47,57 +67,111 @@ $(document).ready(function()
 		ele.attr('title',date.getLargeChatStamp()).text(date.getSmallChatStamp());
 	});
 	setTimeout(function() {
-		if(!window.webkitNotifications) return;
-		switch(window.webkitNotifications.checkPermission())
-		{
-		case 0: break;
-		default:
-			$('#enableNotifs').on('click',function() {
-				window.webkitNotifications.requestPermission(function() { 
-					$('#enableNotifs').hide();
-				});
-			});
-			$('#enableNotifs').show();
-		}
+		if(!window.Notification || typeof(Storage) == "undefined") return;
+		
+		if(Notification.permissionLevel() == "granted") $('#notify_chat').val(localStorage['notify_chat']);
+
+		$('#notify_chat').on('change',function() {
+			var enable = function() { localStorage['notify_chat'] = $('#notify_chat').val(); }
+			switch(Notification.permissionLevel())
+			{
+			case "granted":
+				enable();
+				break;
+			case "denied":
+				alert('Please enable Notifications in your browser\'s settings');
+				break;
+			case "default":
+			default:
+				//Notification.requestPermission(function() { if(Notification.permissionLevel() == "granted") enable(); }); // Crashes in Chrome 21
+				window.webkitNotifications.requestPermission(function() { if(Notification.permissionLevel() == "granted") enable(); });
+			}
+		});
+
+		$('#enableNotifs').show();
 	},1);
 });
 </script>
 <style>
-#template { display: none; }
-#messages,#chatters { list-style-type: none; margin: 0px; padding: 0px; }
-#messages a.username,#chatters a.username { color: #317396; text-decoration: none; }
-#messages a.username:hover,#chatters a.username:hover { color: #06C; }
-#chatters { color: #317396; }
-#chatters li { line-height: 16px; padding-bottom: 2px; }
-#chatters li img { width:16px;height:16px;vertical-align:middle; }
-#messages
-{
+#template {
+	display: none;
+}
+
+#messages,#chatters {
+	list-style-type: none;
+	margin: 0px;
+	padding: 0px;
+}
+
+#messages a.username,#chatters a.username {
+	color: #317396;
+	text-decoration: none;
+}
+
+#messages a.username:hover,#chatters a.username:hover {
+	color: #06C;
+}
+
+#chatters {
+	color: #317396;
+}
+
+#chatters li {
+	line-height: 16px;
+	padding-bottom: 2px;
+}
+
+#chatters li img {
+	width: 16px;
+	height: 16px;
+	vertical-align: middle;
+}
+
+#messages {
 	display: block;
 	width: 100%;
 	height: 300px;
 	overflow-y: scroll;
 	overflow-x: hidden;
 }
-#messages li { color: white; padding-bottom: 3px; font-size: 6pt; }
-#messages li * { font-size: 10pt; }
-#messages .timestamp { color: #999; }
-#messages .username { color: #317396; }
-#messages .message { color: #444; }
-#chatForm
-{
+
+#messages li {
+	color: white;
+	padding-bottom: 3px;
+	font-size: 6pt;
+}
+
+#messages li * {
+	font-size: 10pt;
+}
+
+#messages .timestamp {
+	color: #999;
+}
+
+#messages .username {
+	color: #317396;
+}
+
+#messages .message {
+	color: #444;
+}
+
+#chatForm {
 	width: 100%;
 }
-#chatForm input
-{
+
+#chatForm input {
 	width: 645px;
 }
-#chatForm button
-{
+
+#chatForm button {
 	width: 50px;
 }
+
 #announcement {
-    border: 1px solid #DDD;
-    padding: 5px;
-    margin: 5px 20px 0px 20px;
+	border: 1px solid #DDD;
+	padding: 5px;
+	margin: 5px 20px 0px 20px;
 }
 </style>
