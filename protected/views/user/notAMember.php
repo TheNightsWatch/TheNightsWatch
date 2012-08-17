71d55796a175c428810fc4827963b01ba6348568
Navarr Barnier <me@navarr.me>
2012-08-17 01:12:50 -0400

    Switch to Internal Skin Viewer

--- protected/views/user/notAMember.php
+++ protected/views/user/notAMember.php
@@ -1,5 +1,5 @@
 <div style="text-align:center;">
-	<iframe src="http://minez.net/skin/index.php?user=<?= urlencode($name) ?>&amp;refresh" title="skin" width="200px" height="200px"></iframe>
+	<iframe src="<?php echo Yii::app()->baseUrl . '/external/skin?user='.urlencode($name) ?>&amp;refresh" title="skin" width="200px" height="200px"></iframe>
 	<br />
 	<p style="font-style:italic;"><?= htmlspecialchars($name) ?> has yet to join <strong>The Night's Watch</strong>.  Perhaps you should <a href="http://www.minez.net/?action=playersearch&player=<?= urlencode($name) ?>">recruit them</a>.<br />After all, there is honor in joining The Night's Watch.</p>
 </div>
