81aabb3deba3431ac327610ea95bac3a090da201
Navarr Barnier <me@navarr.me>
2012-08-24 23:40:55 -0400

    Map Information Pop-Up

--- map/places.js
+++ map/places.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	$.getJSON('/map/places',function(data){
 		console.log('Got Places');
 		var cts = overviewer.mapView.options.currentTileSet;
+		document.placeMarkers = [];
+		var iWindow = new google.maps.InfoWindow();
 		for(var i in data)
 		{
 			var place = data[i];
@@ -21,8 +23,15 @@ $(document).ready(function(){
 			if(place.type == 'TOWN' && (place.loot & 4) == 4) icon = iconDIR + 'town_mil.png';
 			if(place.type == 'TOWN' && (place.loot & 6) == 6) icon = iconDIR + 'town_both.png';
 			
+			var info = '<div style="width:300px;"><strong>' + place.name + '</strong><br />Known to Have:<ul>';
+			if((place.loot & 1) == 1) info = info + '<li>Civilian Loot</li>';
+			if((place.loot & 2) == 2) info = info + '<li>Food</li>';
+			if((place.loot & 4) == 4) info = info + '<li>Military Gear</li>';
+			if((place.loot & 8) == 8) info = info + '<li>Water</li>';
+			info = info + '</ul></div>';
+			
 			var img = new google.maps.MarkerImage(icon);
-			new google.maps.Marker({
+			var marker = new google.maps.Marker({
 				position: overviewer.util.fromWorldToLatLng(place.x,50,place.z,cts),
 				map: overviewer.map,
 				title: place.name,
@@ -30,6 +39,17 @@ $(document).ready(function(){
 				zIndex: 999,
 				icon: img,
 			});
+			
+			createInfoWindow(iWindow,marker,info);
 		}
 	});
 });
+
+function createInfoWindow(iWindow,marker,info)
+{
+	google.maps.event.addListener(marker,'click',function() {
+		iWindow.content = info;
+		iWindow.open(marker.getMap(), marker);
+		marker.getMap().panTo(marker.position);
+	});
+}
