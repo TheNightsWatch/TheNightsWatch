dc0f50dff326b74e018a92e56827b7413f580c93
Navarr Barnier <me@navarr.me>
2012-09-01 12:42:48 -0400

    Add Message to the profile for verified members

--- protected/views/user/view.php
+++ protected/views/user/view.php
@@ -1,5 +1,6 @@
 <header>
 	<h1><?= CHtml::encode($user->ign) ?></h1>
 	<div style="text-align: right;"><em><?= $user->title ?> of the Night's Watch</em></div>
+	<?php if($user->verified): ?><div style="text-align: right;font-weight:bold;"><em>Verified Member</em></div><?php endif; ?>
 </header>
 <p>Information will go here once/if I acquire some sort of API for MineZ Stats</p>
