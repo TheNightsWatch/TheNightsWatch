ada51538999301ad4279717443f7de01efb84d26
Navarr Barnier <me@navarr.me>
2012-09-01 00:34:23 -0400

    Add Disabled & Verified Accounts

---
+++ protected/migrations/m120901_042236_add_disabled_verified.php
@@ -0,0 +1,16 @@
+<?php
+
+class m120901_042236_add_disabled_verified extends CDbMigration
+{
+	public function safeUp()
+	{
+	    $this->alterColumn('user','deserter',"ENUM('NO','DESERTER','LEFT') NOT NULL DEFAULT 'NO'");
+	    $this->addColumn('user','verified','BOOLEAN NOT NULL DEFAULT 0');
+	}
+
+	public function safeDown()
+	{
+	    $this->alterColumn('user','deserter',"ENUM('NO','DESERTER','LEFT') NOT NULL DEFAULT 'NO'");
+	    $this->dropColumn('user','verified');
+	}
+}
