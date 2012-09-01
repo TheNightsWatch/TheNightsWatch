429e9489d0aa0c27c58840377b962717e1c83832
Navarr Barnier <me@navarr.me>
2012-09-01 00:35:27 -0400

    Forgot to actually add disabled.

--- protected/migrations/m120901_042236_add_disabled_verified.php
+++ protected/migrations/m120901_042236_add_disabled_verified.php
@@ -4,7 +4,7 @@ class m120901_042236_add_disabled_verified extends CDbMigration
 {
 	public function safeUp()
 	{
-	    $this->alterColumn('user','deserter',"ENUM('NO','DESERTER','LEFT') NOT NULL DEFAULT 'NO'");
+	    $this->alterColumn('user','deserter',"ENUM('NO','DESERTER','LEFT','DISABLED') NOT NULL DEFAULT 'NO'");
 	    $this->addColumn('user','verified','BOOLEAN NOT NULL DEFAULT 0');
 	}
 
