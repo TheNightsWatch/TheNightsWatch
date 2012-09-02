c5cfabb0407fac4bdfacbe9047c53f69b4a0cbb8
Navarr Barnier <me@navarr.me>
2012-09-02 14:51:50 -0400

    Clarify Userlist

--- protected/views/user/index.php
+++ protected/views/user/index.php
@@ -1,3 +1,4 @@
+<p>Verified Members of the Watch in order by join date:</p>
 <ul style="list-style-type:none;">
 <?php foreach($users as $user): ?>
 	<li><img src="<?= $user->headUrl(16); ?>" style="width:16px;height:16px;vertical-align:middle;" /> <a href="<?= $this->createUrl('/user/view',array('unique' => $user->ign)); ?>"><?= CHtml::encode($user->ign); ?></a>, <em><?= $user->title; ?> of the Night's Watch</em></li>
