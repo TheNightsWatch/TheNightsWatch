e5f45d3ce37fbf57496104c1065dfc845b044c45
Navarr Barnier <me@navarr.me>
2012-10-13 14:21:11 -0400

    Removing Teamspeak from the Voice Chat page.

--- protected/views/site/teamspeak.php
+++ protected/views/site/teamspeak.php
@@ -1,11 +1,4 @@
-<p>If you would like to chat with us in person, you have two options:</p>
-<strong>Teamspeak</strong>
-<p>The Night's Watch is graciously hosted on <a href="<?php echo $this->createUrl('user/view',array('unique' => 'Angellus_Mortis')); ?>">Angellus_Mortis</a>'s Teamspeak Server</p>
-<blockquote>
-    <a href="<?php echo $uri_teamspeak; ?>"><?php echo $teamspeak; ?></a>
-</blockquote>
-<strong>Mumble</strong>
-<p>In addition to Teamspeak, which may have a smaller limit - The Night's Watch is also graciously hosted on MineZ's Mumble Server</p>
+<p>The Night's Watch is also graciously hosted on MineZ's Mumble Server</p>
 <blockquote>
     <a href="<?php echo $uri_mumble; ?>"><?php echo $mumble; ?></a>
 </blockquote>
