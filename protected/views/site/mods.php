65bc1a350bbd3559d89bf2eb0ba7951f70d12507
Navarr Barnier <me@navarr.me>
2012-11-14 23:02:51 -0500

    Updating mod to 1.4.4

--- protected/views/site/mods.php
+++ protected/views/site/mods.php
@@ -1,8 +1,8 @@
 <p>We are no longer hand-holding mod installation.  You're on your own now.</p>
-<p>Our mod works with ModLoader and Forge Mod Loader for [1.4.2]</p>
+<p>Our mod works with ModLoader for [1.4.4]</p>
 <p>If you're using <a href="http://www.minecraftforum.net/topic/939149-">Magic Launcher</a>, this is the order things should be in:</p>
 <ol>
-    <li><a href="http://files.minecraftforge.net/minecraftforge-universal-recommended.zip">Minecraft Forge</a></li>
+    <li><a href="http://www.minecraftforum.net/topic/75440-">Minecraft Forge</a></li>
     <li><a href="http://www.minecraftforum.net/topic/249637-">Optifine</a></li>
     <li>(optional) <a href="http://www.minecraftforum.net/topic/482147-">Rei's Minimap</a></li>
     <li><a href="http://www.minez-nightswatch.com/other/nightswatch.zip">The Night's Watch Mod</a></li>
