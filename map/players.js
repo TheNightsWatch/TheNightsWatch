6a521ae121cd62d13beaa158e838ca9cef0a112b
Navarr Barnier <me@navarr.me>
2013-05-26 19:27:36 -0400

    Fix all but one jslint error.

--- map/players.js
+++ map/players.js
@@ -1,94 +1,99 @@
 var TNWPlugin = function () {
+    'use strict';
     // The Night's Watch Player Markers
     // A Heavily modified version of Overviewer's PlayerMarkers
     // specifically designed for MinezMap.com
 
-    var $ = jQuery;
-    var JSONFile = 'https://minez-nightswatch.com/map/players';
-    var refreshTime = 2;
-    var avatarServer = 'https://minotar.net/helm/<playername>/<size>.png';
+    var $ = jQuery,
+        JSONFile = 'https://minez-nightswatch.com/map/players',
+        refreshTime = 2,
+        avatarServer = 'https://minotar.net/helm/<playername>/<size>.png',
+        players = {};
+
     /**
      * { name: { name: "", rank: "", server: "", location: {x: 0, y: 0, z: 0}, marker: obj } }
      */
-    var players = {};
 
     function loadPlayers() {
-        var cacheBuster = (new Date).getTime();
         /*
          * We'll use <script id="nightswatch-update"> for our JSONP, so that
          * we don't accidentally bog down the DOM.
          */
-        var script = $('#nightswatch-update');
+        var cacheBuster = (new Date()).getTime(),
+            script = $('#nightswatch-update'),
+            url = JSONFile + '?jsonp=TNWPluginInstance.updatePlayers&_time=' + cacheBuster;
         if (script.length) {
             script.remove();
         }
-        var url = JSONFile + '?jsonp=TNWPluginInstance.updatePlayers&_time=' + cacheBuster;
         $('body').append('<script src="' + url + '" id="nightswatch-update"></script>');
     }
-
-    this.updatePlayers = function (data) {
-        var updated = {};
-        for (var i in data) {
-            /**
-             * @property string timestamp
-             * @property int id
-             * @property string msg
-             * @property string display
-             * @property string x
-             * @property string y
-             * @property string z
-             * @property string world
-             * @property string server
-             * @property string rank
-             * @type {*}
-             */
-            var item = data[i];
-            var player;
-            if (players[item.display]) {
-                player = players[item.display];
-            } else {
-                players[item.display] = {};
-                player = players[item.display];
-            }
-            player.name = item.display;
-            player.rank = item.rank;
-            player.server = item.server;
-            player.location = {x: item.x, y: item.y, z: item.z };
-            var latlng = overviewer.util.fromWorldToLatLng(player.location.x, player.location.y, player.location.z, 0);
-            latlng = [latlng.lat, latlng.lng];
-            if (player.marker) {
-                player.marker.setLatLng(latlng);
-            } else {
-                var icon = L.icon({
-                    iconUrl: getAvatarURL(player.name, 16),
-                    iconRetinaUrl: getAvatarURL(player.name, 32),
-                    iconSize: [16, 16]
-                });
-                player.marker = L.marker(latlng, {
-                    icon: icon,
-                    title: player.name,
-                    riseOnHover: true
-                });
-                player.marker.addTo(overviewer.map);
-                player.marker.on('click', function() {
-                    mapUi.expandSidebar();
-                    mapUi.openUrl('https://minez-nightswatch.com/user/' + player.name);
-                })
-            }
-            updated[player.name] = true;
-        }
-        for (var i in players) {
-            if (!updated[players[i].name]) {
-                overviewer.map.removeLayer(players[i].marker);
-                delete players[i];
-            }
-        }
-    };
 
     function getAvatarURL(name, size) {
         return avatarServer.replace('<playername>', name).replace('<size>', size);
     }
 
-    var timer = setInterval(loadPlayers, 1000 * refreshTime);
+    this.updatePlayers = function (data) {
+        var updated = {}, i, item, player, latlng, icon;
+        for (i in data) {
+            if (data.hasOwnProperty(i)) {
+                /**
+                 * @property string timestamp
+                 * @property int id
+                 * @property string msg
+                 * @property string display
+                 * @property string x
+                 * @property string y
+                 * @property string z
+                 * @property string world
+                 * @property string server
+                 * @property string rank
+                 * @type {*}
+                 */
+                item = data[i];
+                if (players[item.display]) {
+                    player = players[item.display];
+                } else {
+                    players[item.display] = {};
+                    player = players[item.display];
+                }
+                player.name = item.display;
+                player.rank = item.rank;
+                player.server = item.server;
+                player.location = {x: item.x, y: item.y, z: item.z };
+                latlng = overviewer.util.fromWorldToLatLng(player.location.x, player.location.y, player.location.z, 0);
+                latlng = [latlng.lat, latlng.lng];
+                if (player.marker) {
+                    player.marker.setLatLng(latlng);
+                } else {
+                    icon = L.icon({
+                        iconUrl: getAvatarURL(player.name, 16),
+                        iconRetinaUrl: getAvatarURL(player.name, 32),
+                        iconSize: [16, 16]
+                    });
+                    player.marker = L.marker(latlng, {
+                        icon: icon,
+                        title: player.name,
+                        riseOnHover: true
+                    });
+                    player.marker.addTo(overviewer.map);
+                    player.marker.on('click', function () {
+                        mapUi.expandSidebar();
+                        mapUi.openUrl('https://minez-nightswatch.com/user/' + player.name);
+                    });
+                }
+                updated[player.name] = true;
+            }
+        }
+        for (i in players) {
+            if (players.hasOwnProperty(i)) {
+                if (!updated[players[i].name]) {
+                    overviewer.map.removeLayer(players[i].marker);
+                    delete players[i];
+                }
+            }
+        }
+    };
+
+    window.setInterval(loadPlayers, 1000 * refreshTime);
 };
 window.TNWPluginInstance = new TNWPlugin();
